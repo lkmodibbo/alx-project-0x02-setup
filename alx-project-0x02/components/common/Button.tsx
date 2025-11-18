a21bce9ac5d41cbf7e0a94f4fc86348d4698fd49
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-11-18 11:28:12 +0100

    created and use button component

---
+++ alx-project-0x02/components/common/Button.tsx
@@ -0,0 +1,17 @@
+import { type ButtonProps } from "@/interfaces";
+
+export default function Button({ size, shape, label }: ButtonProps) {
+  const sizeClasses = {
+    small: "px-2 py-1 text-sm",
+    medium: "px-4 py-2 text-base",
+    large: "px-6 py-3 text-lg",
+  };
+
+  return (
+    <button
+      className={`bg-blue-600 text-white ${sizeClasses[size]} ${shape}`}
+    >
+      {label}
+    </button>
+  );
+}
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,7 +1,15 @@
+import Button from "@/components/common/Button";
+
 export default function AboutPage() {
   return (
-    <div>
-      <h1>About Us Page</h1>
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">About Page</h1>
+
+      <div className="flex gap-4">
+        <Button size="small" label="Small Button" shape="rounded-sm" />
+        <Button size="medium" label="Medium Button" shape="rounded-md" />
+        <Button size="large" label="Large Button" shape="rounded-full" />
+      </div>
     </div>
   );
 }
